72d5006d52dfbf9851ee90ecbb274403e6576c3e
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-23 22:31:28 +0500

    navbar upd

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
+"use client";
+
 function Navbar() {
   return (
-    <div className="navbar bg-primary text-primary-content gap-4">
-      <button className="btn btn-ghost text-xl">Calculator</button>
-      <button className="btn btn-ghost text-xl">Contacts</button>
+    <div className="w-full flex items-center gap-4 md:gap-12">
+      <button className="btn btn-neutral text-xl">Calculator</button>
+      <button className="btn btn-neutral text-xl">Contacts</button>
     </div>
   );
 }
