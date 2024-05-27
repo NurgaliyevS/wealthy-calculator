dcf87f55950ce232536da6467c70051de3855f66
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-27 18:45:30 +0500

    upd

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Bar, YAxis, XAxis, CartesianGrid, Tooltip, BarChart } from "recharts";
+import { Bar, YAxis, XAxis, CartesianGrid, Tooltip, BarChart, ResponsiveContainer } from "recharts";
 
 const ChartComponent = ({ chartData }) => {
   const hasContributions = chartData.some(
@@ -9,32 +9,38 @@ const ChartComponent = ({ chartData }) => {
   );
 
   return (
-    <BarChart width={600} height={300} data={chartData}>
-      <CartesianGrid strokeDasharray="0.1 0.1" />
-      <YAxis />
-      <XAxis dataKey="year" />
-      <Tooltip formatter={(value) => `$${value.toLocaleString()}`} />
-      <Bar
-        dataKey="startingAmount"
-        stackId="a"
-        fill="#8884d8"
-        name="Starting Amount"
-      />
-      {hasContributions && (
-        <Bar
-          dataKey="totalContributions"
-          stackId="a"
-          fill="#82ca9d"
-          name="Total Contributions"
-        />
-      )}
-      <Bar
-        dataKey="totalInterest"
-        stackId="a"
-        fill="#ffc658"
-        name="Total Interest Earned"
-      />
-    </BarChart>
+    <div className="w-full h-64 lg:h-96">
+      <div className="w-full lg:w-3/5 h-full">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart data={chartData}>
+            <CartesianGrid strokeDasharray="0.1 0.1" />
+            <YAxis />
+            <XAxis dataKey="year" />
+            <Tooltip formatter={(value) => `$${value.toLocaleString()}`} />
+            <Bar
+              dataKey="startingAmount"
+              stackId="a"
+              fill="#8884d8"
+              name="Starting Amount"
+            />
+            {hasContributions && (
+              <Bar
+                dataKey="totalContributions"
+                stackId="a"
+                fill="#82ca9d"
+                name="Total Contributions"
+              />
+            )}
+            <Bar
+              dataKey="totalInterest"
+              stackId="a"
+              fill="#ffc658"
+              name="Total Interest Earned"
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
   );
 };
 
