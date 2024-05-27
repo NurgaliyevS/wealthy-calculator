848fb7cc374b02ad07e7a08bd135a22afc6528e9
NurgaliyevS <nurgaliyev000@gmail.com>
2024-05-27 21:12:18 +0500

    upd

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Bar,
   YAxis,
@@ -12,6 +12,8 @@ import {
 } from "recharts";
 
 const ChartComponent = ({ chartData }) => {
+  const [requiresMargin, setRequiresMargin] = useState(false);
+
   const hasContributions = chartData.some(
     (data) => data?.totalContributions > 0
   );
@@ -23,12 +25,26 @@ const ChartComponent = ({ chartData }) => {
     return `$${tickItem.toLocaleString()}`;
   };
 
+  useEffect(() => {
+    // Check if any y-axis labels exceed 1000
+    const requiresMarginAdjustment = chartData.some(
+      (data) =>
+        data.startingAmount >= 1000000 ||
+        data.totalInterest >= 1000000 ||
+        data.totalContributions >= 1000000
+    );
+    setRequiresMargin(requiresMarginAdjustment);
+  }, [chartData]);
+
   return (
     <>
       <div className="w-full h-64 lg:h-96">
         <div className="w-full lg:w-3/5 h-full">
           <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={chartData}>
+            <BarChart
+              data={chartData}
+              margin={{ left: requiresMargin ? 30 : 0 }}
+            >
               <CartesianGrid strokeDasharray="0.1 0.1" />
               <YAxis tickFormatter={formatYAxis} />
               <XAxis dataKey="year" />
