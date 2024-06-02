c4eac80c636d72580fa6341fa58d261d3a019329
NurgaliyevS <nurgaliyev000@gmail.com>
2024-06-02 16:33:57 +0500

    upd

--- components/Main.js
+++ components/Main.js
@@ -13,7 +13,7 @@ function Main() {
             </span>
           </h1>
           <p className="text-lg opacity-80 leading-relaxed">
-            The Wealth Calculator: Everything you need to invest money wisely
+            Everything you need to invest money wisely
           </p>
         </div>
       </section>
