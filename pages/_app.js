7172d6bcabef77fe3840e6ebd82d00cca08898de
NurgaliyevS <nurgaliyev000@gmail.com>
2024-10-23 17:33:44 +0500

    upd

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import "@/styles/globals.css";
-
 import PlausibleProvider from "next-plausible";
+import Script from 'next/script';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script 
+        defer
+        data-domain="weeealth.com" 
+        src="https://plausible.io/js/script.hash.outbound-links.js"
+      />
+      <Script>
+        {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
+      </Script>
       <PlausibleProvider domain="weeealth.com">
         <Component {...pageProps} />
         <ToastContainer
